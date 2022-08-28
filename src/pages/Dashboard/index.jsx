bc72ce13f4de88da8483ce7e04d5af59514b0fa2
gabriel-williams <gabrielwilliamcps@gmail.com>
2022-08-28 18:00:22 -0300

    refactor: Add component PageAdminLayout

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,27 +1,17 @@
-import { useState } from "react";
-import { DashbordCard, HeaderAdmin, SideMenu } from "../../components";
-import { Container, ContentWrapper, DashbordCardContainer } from "./styles";
+import { DashbordCard, PageAdmin } from "../../components";
+import { DashbordCardContainer } from "./styles";
 
 export function Dashboard() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
-    <Container>
-      <HeaderAdmin menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <SideMenu openMenu={menuOpen} className={menuOpen ? "menuOpen" : ""} />
-      <ContentWrapper>
-        <DashbordCardContainer>
-          <DashbordCard
-            title="Quantidade de produtos cadastrados"
-            amount={"1.000"}
-          />
-          <DashbordCard
-            title="Quantidade de produtos vendidos"
-            amount={"500"}
-          />
-          <DashbordCard title="Faturamento" amount={"R$: 5000.00"} />
-        </DashbordCardContainer>
-      </ContentWrapper>
-    </Container>
+    <PageAdmin>
+      <DashbordCardContainer>
+        <DashbordCard
+          title="Quantidade de produtos cadastrados"
+          amount={"1.000"}
+        />
+        <DashbordCard title="Quantidade de produtos vendidos" amount={"500"} />
+        <DashbordCard title="Faturamento" amount={"R$: 5000.00"} />
+      </DashbordCardContainer>
+    </PageAdmin>
   );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,20 +1,5 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  height: 100vh;
-  display: grid;
-  grid-template-columns: auto 1fr;
-  grid-template-rows: 12.3rem auto;
-  grid-template-areas:
-    "header header"
-    "sidemenu content";
-`;
-
-export const ContentWrapper = styled.main`
-  grid-area: content;
-  padding: 3rem;
-`;
-
 export const DashbordCardContainer = styled.div`
   width: 100%;
   display: flex;
