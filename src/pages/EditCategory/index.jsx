3577b0c9df7c85207f4ecf8009c1e44366ce00c7
gabriel-williams <gabrielwilliamcps@gmail.com>
2022-09-27 19:45:23 -0300

    feat: edit category with api

--- src/pages/EditCategory/index.jsx
+++ src/pages/EditCategory/index.jsx
@@ -1,39 +1,83 @@
-import { BackButton, Button, InputLabel, Section } from "../../components";
+import {
+  BackButton,
+  Button,
+  InputLabel,
+  Section,
+  LoadingAnimate
+} from "../../components";
 import { Container } from "./styles";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { categoryFields } from "../../utils/validations";
 import { toast } from "react-toastify";
+import { useNavigate, useParams } from "react-router-dom";
+import { api } from "../../services/api";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export function EditCategory({ ...rest }) {
+  const [category, setCategory] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const { id } = useParams();
+
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm({ resolver: yupResolver(categoryFields) });
 
-  function handleEditCategory(data) {
-    toast.success("Categoria editada com sucesso!");
-    console.log(data);
+  async function handleEditCategory(data) {
+    try {
+      await api.put(`/categories/${id}`, {
+        name: data.name
+      });
+
+      toast.success("Categoria editada com sucesso!");
+      navigate("/categories");
+    } catch (error) {
+      if (error.response.data.message) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("Erro ao editar categoria.");
+      }
+    }
   }
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      setIsLoading(true);
+
+      const { data } = await api.get(`/categories/${id}`);
+      setCategory(data.name);
+      setIsLoading(false);
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <Container {...rest}>
       <BackButton />
       <Section title="Editar categoria">
-        <form onSubmit={handleSubmit(handleEditCategory)}>
-          <InputLabel
-            placeholder="Name"
-            id="name"
-            name="name"
-            label="Nome"
-            register={register}
-            error={errors.name}
-            borderError={true}
-            textError={true}
-          />
-
-          <Button name="Salvar" />
-        </form>
+        {isLoading && category ? (
+          <LoadingAnimate />
+        ) : (
+          <form onSubmit={handleSubmit(handleEditCategory)}>
+            <InputLabel
+              placeholder="Name"
+              id="name"
+              name="name"
+              label="Nome"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              register={register}
+              error={errors.name}
+              borderError={true}
+              textError={true}
+            />
+            <Button name="Salvar" />
+          </form>
+        )}
       </Section>
     </Container>
   );
